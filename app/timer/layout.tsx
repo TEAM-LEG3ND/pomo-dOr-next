ef6aa5710458900c0618e817a63d0ef030ee9937
dkile <dkile532@naver.com>
2023-08-20 03:22:24 +0900

    Add prefetching clock background image

--- app/timer/layout.tsx
+++ app/timer/layout.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Providers from "./providers";
+import { useQueryClient } from "@tanstack/react-query";
+import { clockBackgroundImageQuery } from "@/components/timer/timeTimer/TimeTimerBackground";
 
 interface Props {
   children: ReactNode;
 }
 
 function Layout({ children }: Props) {
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const prefetchClockBackground = async () => {
+      await queryClient.prefetchQuery(clockBackgroundImageQuery);
+    };
+    prefetchClockBackground();
+  }, []);
+
   return <Providers>{children}</Providers>;
 }
 
--- components/timer/timeTimer/TimeTimerBackground.tsx
+++ components/timer/timeTimer/TimeTimerBackground.tsx
@@ -1,14 +1,32 @@
 import { ReactNode, memo } from "react";
-import Icon from "../../common/icon/Icon";
+import { fetchIcon } from "@/apis/common/api";
+import { useQuery } from "@tanstack/react-query";
+import { S3_BASE_URL } from "@/constants/common";
 
 interface Props {
   children: ReactNode;
 }
 
+export const clockBackgroundImageQuery = {
+  queryKey: ["image", "clock-background"],
+  queryFn: () => fetchIcon("clock", { baseUrl: S3_BASE_URL }),
+};
+
 function TimeTimerBackground({ children }: Props) {
+  const { data: clockBackgroundImage } = useQuery(clockBackgroundImageQuery);
+
   return (
     <div className="relative h-full w-full">
-      <Icon name="clock" className="absolute left-0 top-0 h-full w-full" />
+      <span
+        className="absolute left-0 top-0 h-full w-full"
+        dangerouslySetInnerHTML={
+          clockBackgroundImage
+            ? {
+                __html: clockBackgroundImage,
+              }
+            : undefined
+        }
+      />
       {children}
     </div>
   );
